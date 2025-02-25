2ed38b819c863b3f54c64fc504091faa6606fd38
Artem <vsezdes021@gmail.com>
2025-02-25 21:18:29 +0200

    added pagination

--- src/js/paginator.js
+++ src/js/paginator.js
@@ -1,18 +1,16 @@
 const apiUrl = 'https://app.ticketmaster.com/discovery/v2/events.json';
-const apiKey = 'nGm4oZ3VNxvLAyNFXF3x88MrBbSefbXA'; // Заміни на свій API ключ
+const apiKey = 'nGm4oZ3VNxvLAyNFXF3x88MrBbSefbXA';
 
-// Функція для запиту подій
 async function fetchEvents(page = 1) {
   const response = await fetch(`${apiUrl}?page=${page}&apikey=${apiKey}`);
   const data = await response.json();
   return data;
 }
 
-// Функція для розрахунку сторінок пагінації
 function calculatePages(totalResults, pageSize, currentPage) {
   const totalPages = Math.ceil(totalResults / pageSize);
-  const startPage = Math.max(currentPage - 2, 1); // Почати з 2 сторінок до поточної
-  const endPage = Math.min(currentPage + 2, totalPages); // Закінчити 2 сторінки після поточної
+  const startPage = Math.max(currentPage - 2, 1); 
+  const endPage = Math.min(currentPage + 2, totalPages); 
 
   return {
     totalPages,
@@ -26,27 +24,61 @@ function calculatePages(totalResults, pageSize, currentPage) {
   };
 }
 
-// Шаблон Handlebars для пагінації
 const templateSource = `
-      <ul class="paginator-list">
-        {{#if firstPageBlock}}
-          <li data-page="1">1</li>
-          <li class="previous-5">...</li>
-        {{/if}}
-        
-        {{#each pagesToShow}}
-          <li data-page="{{this}}">{{this}}</li>
-        {{/each}}
+  <ul class="paginator-list">
+    {{#if firstPageBlock}}
+      <li class="paginator-items" data-page="1">1</li>
+      <li class="paginator-item" class="previous-5">...</li>
+    {{/if}}
+    
+    {{#each pagesToShow}}
+      <li class="paginator-items" data-page="{{this}}">{{this}}</li>
+    {{/each}}
 
-        {{#if lastPageBlock}}
-          <li class="next-5">...</li>
-          <li data-page="{{lastPage}}">{{lastPage}}</li>
-        {{/if}}
-      </ul>
-    `;
+    {{#if lastPageBlock}}
+      <li class="paginator-item" class="next-5">...</li>
+      <li class="paginator-items" data-page="{{lastPage}}">{{lastPage}}</li>
+    {{/if}}
+  </ul>
+`;
 const template = Handlebars.compile(templateSource);
 
-// Основна функція для рендерингу пагінації
+const eventsTemplateSource = `
+  <ul class="list-card-main">
+    {{#each this}}
+      <li class="item-card-main">
+        <div>
+          {{#if images.[0]}}
+            <img
+              class="img-card"
+              src="{{images.[0].url}}"
+              alt="{{name}}"
+              width="180"
+              height="120"
+            />
+          {{/if}}
+        </div>
+        <h2 class="name-card">{{name}}</h2>
+        <p class="dates-card"><b></b> {{dates.start.localDate}}</p>
+        <p class="country-card">
+          {{_embedded.venues.[0].city.name}}, 
+          {{_embedded.venues.[0].country.name}}
+        </p>
+      </li>
+    {{/each}}
+  </ul>
+`;
+const eventsTemplate = Handlebars.compile(eventsTemplateSource);
+
+function renderEvents(events) {
+  const eventsContainer = document.querySelector('.cards-list .container');
+  eventsContainer.innerHTML = ''; 
+
+  const eventsHtml = eventsTemplate(events);
+
+  eventsContainer.innerHTML = eventsHtml;
+}
+
 async function renderPagination(currentPage = 1) {
   const eventsData = await fetchEvents(currentPage);
   const paginationData = calculatePages(
@@ -55,22 +87,24 @@ async function renderPagination(currentPage = 1) {
     currentPage
   );
 
-  // Генеруємо HTML
-  const html = template(paginationData);
+  const paginationHtml = template(paginationData);
 
-  // Вставляємо в контейнер
-  document.querySelector('.paginator').innerHTML = html;
+  document.querySelector('.paginator').innerHTML = paginationHtml;
+
+  renderEvents(eventsData._embedded.events);
 }
 
-// Викликаємо функцію для рендерингу пагінації для 1-ї сторінки
 renderPagination(1);
 
-// Обробка кліків на сторінки пагінації
-document.querySelector('.paginator').addEventListener('click', function (event) {
-  if (event.target.tagName === 'LI') {
-    const page = parseInt(event.target.getAttribute('data-page'));
-    if (page) {
-      renderPagination(page);
+document
+  .querySelector('.paginator')
+  .addEventListener('click', function (event) {
+    if (event.target.tagName === 'LI') {
+      const page = parseInt(event.target.getAttribute('data-page'));
+      if (page) {
+        renderPagination(page);
+      }
     }
-  }
-});
+  });
+
+
